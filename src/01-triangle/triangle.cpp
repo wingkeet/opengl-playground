178394aba34d43c93dcc07f3dfb0d7dbfdfacea4
wingkeet <leongwingkeet@gmail.com>
2022-12-24 23:03:04 +0800

    Fix window title.

--- src/01-triangle/triangle.cpp
+++ src/01-triangle/triangle.cpp
@@ -146,7 +146,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "02-cube", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "01-triangle", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
