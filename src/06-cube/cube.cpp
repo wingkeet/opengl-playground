a06584361cece7454333484ef8cc399de4f91010
wingkeet <leongwingkeet@gmail.com>
2023-02-27 07:40:17 +0800

    Use MSAA for 08-cube.

--- src/06-cube/cube.cpp
+++ src/06-cube/cube.cpp
@@ -171,6 +171,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, window_title().c_str(), nullptr, nullptr);
     if (!window) {
