f6de75688d9b1432c32bb8cc5a43743bfc3f9423
wingkeet <leongwingkeet@gmail.com>
2023-03-16 17:32:56 +0800

    Cleanup.

--- src/11-pyramid/pyramid.cpp
+++ src/11-pyramid/pyramid.cpp
@@ -122,8 +122,8 @@ static void process_gamepad(GLFWwindow* window)
 static void render(GLFWwindow* window, double current_time)
 {
     // Build model matrix
-    const glm::mat4 identity_matrix{1.0f};
     const float tf = static_cast<float>(current_time);
+    const glm::mat4 identity_matrix{1.0f};
     const glm::mat4 model_matrix = glm::rotate(identity_matrix,
         std::sin(tf) * 2.0f, glm::vec3{0.0f, 1.0f, 0.0f});
 
